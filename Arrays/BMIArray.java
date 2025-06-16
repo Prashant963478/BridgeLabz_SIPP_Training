0523c51c20b8f64340890e0faedb3cdd02c50dce
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-16 11:15:29 +0530

    [Prashant Solanki] Add . find the Body Mass Index (BMI) of all the team members

--- Arrays/BMIArray.java
+++ Arrays/BMIArray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class BMIArray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc  = new Scanner(System.in);
         System.out.print("Enter number of persons: ");
         int n = sc.nextInt();
 
