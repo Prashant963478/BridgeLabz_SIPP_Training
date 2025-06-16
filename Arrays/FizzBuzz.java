0da208ed6530333d493cea8438ef59616ed45b62
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-16 11:20:37 +0530

    [Prashant Solanki] Add . FizzBuzz

--- Arrays/FizzBuzz.java
+++ Arrays/FizzBuzz.java
@@ -5,7 +5,6 @@ public class FizzBuzz {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a positive number: ");
         int n = sc.nextInt();
-
         if (n <= 0) {
             System.out.println("Invalid input");
             return;
