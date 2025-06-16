8089e409b71d5d341623fda8e5c55fd9d2713a27
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-16 11:38:42 +0530

    [Prashant Solanki] Add . NumberCheck

--- ControlFlow/Level-1/NumberCheck.java
+++ ControlFlow/Level-1/NumberCheck.java
@@ -1,7 +1,6 @@
 public class NumberCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter an integer number: ");
         int number = scanner.nextInt();
 
