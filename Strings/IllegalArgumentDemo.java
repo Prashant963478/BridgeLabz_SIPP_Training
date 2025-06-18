516c25a4487d450141f9997423cdf6b55a617397
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-18 11:38:39 +0530

    [Prashant Solanki] Add . program to demonstrate IllegalArgumentException

--- Strings/IllegalArgumentDemo.java
+++ Strings/IllegalArgumentDemo.java
@@ -14,7 +14,6 @@ public class IllegalArgumentDemo {
             System.out.println("Caught RuntimeException: " + e.getMessage());
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
