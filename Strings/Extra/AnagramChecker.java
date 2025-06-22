4b371480c056e6b8cc601565ea69bd3b6b551d0a
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-22 22:11:39 +0530

    [Prashant Solanki] Add .a Java program that accepts two strings from the user and checks if the two strings are anagrams of each other

--- Strings/Extra/AnagramChecker.java
+++ Strings/Extra/AnagramChecker.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 public class AnagramChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first string: ");
-        
+        System.out.print("Enter first string: ");       
         String s1 = sc.nextLine().replaceAll("\\s+", "").toLowerCase();
         System.out.print("Enter second string: ");
         String s2 = sc.nextLine().replaceAll("\\s+", "").toLowerCase();
