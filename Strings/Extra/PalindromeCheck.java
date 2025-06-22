ae1768ec41855b41c03a72b5cdf1761f4a5f9423
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-22 22:15:24 +0530

    [Prashant Solanki] Add . Java program to check if a given string is a palindrome

--- Strings/Extra/PalindromeCheck.java
+++ Strings/Extra/PalindromeCheck.java
@@ -6,7 +6,6 @@ public class PalindromeCheck {
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
         String reversed = "";
-
         
         for (int i = str.length() - 1; i >= 0; i--)
             reversed += str.charAt(i);
