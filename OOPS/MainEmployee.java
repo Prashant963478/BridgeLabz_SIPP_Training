af95f9cf645c0b070465358f2887853e1048de89
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-23 12:01:48 +0530

    [Prashant Solanki] Add .aprogram for employee details

---
+++ OOPS/MainEmployee.java
@@ -0,0 +1,9 @@
+package com.classobject;
+ 
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		 Employee employee = new Employee("prashant", 101, 40000);
+	        employee.displayDetails(); 		
+	}
+}
